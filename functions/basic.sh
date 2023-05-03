bbb70009df509f67ff827b9025e60254817d3f8e
Davy Luiz <contato_davyluiz@hotmail.com>
2023-05-03 16:01:29 -0300

    Alterando funções shell.

--- functions/basic.sh
+++ functions/basic.sh
@@ -3,7 +3,6 @@
 function breakLine(){
     local count=1
     while [ $count -le "$1" ]; do
-        #echo "espaço nº $count"
         echo ""
         count=$((count+1))
     done
@@ -12,8 +11,6 @@ function breakLine(){
 function checkRoot(){
     if [[ "$(id -u)" -ne 0 ]];then
         return 1
-        #printf "A execução requer privilégios!"
-        #exit 1
     fi
 }
 
