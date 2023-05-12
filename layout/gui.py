79ef44131ddc4aa2e8931ce821355b16b72c9a75
Davy Luiz <contato_davyluiz@hotmail.com>
2023-05-11 23:44:13 -0300

    Alteração no layout.

--- layout/gui.py
+++ layout/gui.py
@@ -1,19 +1,21 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 
 root = Tk()
-root.iconbitmap('../assets/cube.ico')
+root.iconbitmap('../assets/ico/cube.ico')
 root.title("SECAD")
-root.configure(background="#F7F7F7")
+root.configure(background="#F6F6F6")
 root.minsize(500, 500)  # width, height
 root.maxsize(500, 500)
 root.geometry("500x500+420+120")  # width x height + x + y
 
 # Create Label in our window
-text = Label(root, text="F label.", bd=0)
+text = Label(root, text="INICIALIZANDO INTERFACE", bd=0)
 text.pack()
-text2 = Label(root, text="Snd label", bd=0)
-text2.pack()
+
+# text2 = Label(root, text="Snd label", bd=0)
+# text2.pack()
 root.mainloop()
 
-root.mainloop()
+
